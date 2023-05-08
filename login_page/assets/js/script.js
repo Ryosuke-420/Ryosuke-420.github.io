2c793ea98ba485046a138656603250c0a3d8001d
Kahli Mohamed Nour <54864888+Ryosuke-420@users.noreply.github.com>
2023-05-08 12:03:59 -0700

    Delete login_page directory

--- login_page/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-var input = document.getElementById("btn");
-input.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    document.getElementById("btn").click();
-  }
-});
