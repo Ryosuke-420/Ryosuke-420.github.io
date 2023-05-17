8131fdf332798c9b24b737db065e3e8e09e8c144
Kahli Mohamed Nour <kahlimohamednour@gmail.com>
2023-05-17 02:22:38 -0700

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,25 @@ $(function() {
     cssEase: 'linear'
       });
 });
+var clickCount = 0;
+var messages = [
+  "Congratulations! You clicked 20 times.",
+  "Great job! You reached 20 clicks.",
+  "Wow! You made it to 20 clicks.",
+  "Awesome! 20 clicks achieved.",
+  "Well done! 20 clicks completed."
+];
+
+function countClicks() {
+  clickCount++;
+  if (clickCount === 20) {
+    displayPopup();
+    clickCount = 0; 
+  }
+}
+
+function displayPopup() {
+  var randomIndex = Math.floor(Math.random() * messages.length);
+  var message = messages[randomIndex];
+  alert(message);
+}
