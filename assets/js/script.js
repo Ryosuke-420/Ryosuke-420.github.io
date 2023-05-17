0336332d5aedfd42123b45a3623e14c24ed56ee7
Kahli Mohamed Nour <kahlimohamednour@gmail.com>
2023-05-17 09:17:09 -0700

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,12 +34,12 @@ document.addEventListener("DOMContentLoaded", function(){
   window.addEventListener('scroll', function() {
       if (window.scrollY > 50) {
         document.getElementById('navbar_top').classList.add('fixed-top');
-        // add padding top to show content behind navbar
+
         navbar_height = document.querySelector('.navbar').offsetHeight;
         document.body.style.paddingTop = navbar_height + 'px';
       } else {
         document.getElementById('navbar_top').classList.remove('fixed-top');
-         // remove padding top from body
+
         document.body.style.paddingTop = '0';
       } 
   });
@@ -59,8 +59,7 @@ function scrollFunction() {
 }
 
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox
+  document.body.scrollTop = 0; 
 }
 
 function formatTime(date) {
@@ -69,7 +68,6 @@ function formatTime(date) {
   return hours + ":" + minutes;
 }
 
-// Display the current date
 var currentDate = new Date();
 var year = currentDate.getFullYear();
 var month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
@@ -77,19 +75,16 @@ var day = ("0" + currentDate.getDate()).slice(-2);
 var formattedDate = year + "/" + month + "/" + day;
 document.getElementById("time").textContent = formattedDate;
 
-// Add mouseover and mouseout event listeners
+
 var timeElement = document.getElementById("time");
 timeElement.addEventListener("mouseover", function() {
-  // Get the current time and format it as "hh:mm"
+
   var currentTime = new Date();
   var formattedTime = formatTime(currentTime);
 
-  // Update the text content with the formatted time and add animation class
   timeElement.textContent = formattedTime;
   timeElement.classList.add("time-hover");
-
-  // Track mouse movement and update position
-  var duration = 3000; // Duration in milliseconds
+  var duration = 3000; 
   var startTime = Date.now();
   var startX = event.clientX;
   var startY = event.clientY;
@@ -113,7 +108,6 @@ timeElement.addEventListener("mouseover", function() {
 });
 
 timeElement.addEventListener("mouseout", function() {
-  // Restore the original formatted date and remove animation class
   timeElement.textContent = formattedDate;
   timeElement.classList.remove("time-hover");
   timeElement.style.transform = "translate(-50%, -50%)";
