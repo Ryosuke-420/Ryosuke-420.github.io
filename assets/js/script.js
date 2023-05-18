48c5b0b75a69fb685d6e6f274f3d75d54bbaf3ae
Kahli Mohamed Nour <kahlimohamednour@gmail.com>
2023-05-18 13:10:34 -0700

    Mobile bug fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,21 +46,6 @@ document.addEventListener("DOMContentLoaded", function(){
 }); 
 
 
-let mybutton = document.getElementById("myBtn");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0; 
-}
 
 function updateTime() {
   var timeDiv = document.getElementById("time");
